7e8681a3cb49b011025f81110f596344a9b0fe9a
gujarathijay <46221554+gujarathijay@users.noreply.github.com>
2023-02-08 11:01:11 -0800

    Update script.js
    Added onload function

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
 window.addEventListener("load",()=>{
-    "use strict";   
-    window.document.getElementById("empForm").addEventListener("submit", (e)=> {
-        e.preventDefault();
-        window.console.log("ID:" + window.document.getElementById("id").value);
+    
+    "use strict";  
+    
+    window.document.getElementById("empForm").addEventListener("submit", (event)=> {
+        
+        event.preventDefault();
+        
+        window.console.log("Employee ID:" + window.document.getElementById("id").value);
+        
         window.console.log("Name:" + window.document.getElementById("name").value);
+        
         window.console.log("Extension:" + window.document.getElementById("ext").value);
+        
         window.console.log("Email:" + window.document.getElementById("email").value);
-        window.console.log("Department:" + window.document.getElementById("department").value);      
+        
+        window.console.log("Department:" + window.document.getElementById("department").value);     
+        
+        
     });
+    
   });
